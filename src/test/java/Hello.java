d124b08cc11a2e9bf507517d483ba3d055d018fc
Maria <prizmaria@gmail.com>
2021-05-22 11:21:37 -0400

    added another inspo message

--- src/test/java/Hello.java
+++ src/test/java/Hello.java
@@ -1,5 +1,6 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Everything is happenign as it supposed to. Timing is perfect");
+        System.out.println("Trust the process and your HP");
     }
 }
